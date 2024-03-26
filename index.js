7be6b346a2da69e54b53fa57a66bee7344063248
Eddy Sureda Heredia <eddysureda2012@gmail.com>
2024-03-26 19:28:45 -0400

    Add Env variables

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+require('dotenv').config()
 const express = require('express')
+
+
+const Note = require('./models/note')
+
 const app = express()
 
 //Using default json-parser as middleware
@@ -24,31 +29,14 @@ const requestLogger = (request, response, next) => {
 app.use(requestLogger)
 */
 
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
