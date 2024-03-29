950033ac3241acfb3ad441c89e26c8d785c6b1b4
Eddy Sureda Heredia <eddysureda2012@gmail.com>
2024-03-29 11:45:56 -0400

    adding validators

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const morgan = require('morgan')
 
 
 const Note = require('./models/note')
@@ -8,6 +9,19 @@ const app = express()
 
 //Using default json-parser as middleware
 app.use(express.json())
+
+morgan.token("input", function getInput(req) {
+  let input  = {};
+  if (req.method === "GET") {
+    input = req.query
+  } else {
+    input = req.body
+  }
+    console.log(input)
+    return JSON.stringify(input)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :input'))
 
 app.use(express.static('dist'))
 
@@ -39,53 +53,65 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {    
-      response.json(note)  
-    } else {    
-      response.status(404).end()  
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+//DELETE a resource
 
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 //POST Add a resource
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+    important: body.important || false,
+  })
 
-  notes = notes.concat(note)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
 
-  response.json(note)
+    .catch(error => next(error))
 })
 
 // END POST Add a resource
+
+// Update a resource
+app.put('/api/notes/:id', (request, response, next) => {
+
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id, 
+
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 //custom middleware after routes to handle non-existent routes
 const unknownEndpoint = (request, response) => {
@@ -93,6 +119,22 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado
+app.use(errorHandler)
 
 
 
